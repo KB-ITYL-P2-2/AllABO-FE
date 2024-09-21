ddff472d490126a6fc978e43e5fd6fb003df93ee
Doyeon <duseh729@naver.com>
2024-09-21 21:18:46 +0900

    tailwind 설정

---
+++ src/pages/MainPage.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="bg-primary-color">
+    메인이얌
+  </div>
+</template>
+
+<script setup></script>
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{vue,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'primary-color' : "#F4BF42",
+        'kb-dark-gray' : "#5F584E"
+      },
+    },
+  },
+  plugins: [],
+};
