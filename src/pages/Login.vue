b0ebb3a2f5520b2a2af1347051de4707412ab39b
yeon-jae <lisanamyj99@gmail.com>
2024-09-26 11:47:29 +0900

    style: 소셜 로그인 버튼 추가

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -15,12 +15,20 @@
                class="h-[50px] w-[400px] rounded-[15px] border border-kb-brown-1  focus:outline-none focus:ring-2 focus:ring-kb-brown-2"/>
         <br>
         <button class="h-[50px] w-[400px] rounded-[15px] bg-kb-brown-1 text-white text- py-2 px-4 focus:ring-kb-brown-2 mt-[45px]">LOGIN</button>
-        <div class="user_info mt-4 flex justify-center">
-          <button @click.prevent="openEmailModal" type="button" class="text-kb-gray-2 text-[16px] underline ">이메일 찾기</button>
-          <button @click.prevent="openPasswordModal" type="button" class="text-kb-gray-2 text-[16px] underline ml-8 ">비밀번호 찾기</button>
-          <button @click="$router.push('/signup-condition')" type="button" class="text-kb-gray-2 text-[16px] underline ml-8 ">회원가입</button>
+        <div class="user_info mt-4 flex ">
+          <button @click.prevent="openEmailModal" type="button" class="text-kb-gray-2 text-[16px] underline ml-9">이메일 찾기</button>
+          <button @click.prevent="openPasswordModal" type="button" class="text-kb-gray-2 text-[16px] underline ml-9 ">비밀번호 찾기</button>
+          <button @click="$router.push('/signup-condition')" type="button" class="text-kb-gray-2 text-[16px] underline ml-9 ">회원가입</button>
         </div>
       </form>
+    </div>
+    <!--간편 로그인-->
+    <div class="flex justify-center mt-[90px]">
+      <hr class="mt-3 w-28"/><p class=" px-10 text-kb-gray-2">간편 로그인 </p> <hr class="mt-3 w-28"/> <br>
+    </div>
+    <div class="flex justify-center mt-[40px]">
+      <button class="w-[55px] h-[55px]  rounded-[50px] mr-8"><img src="../assets/images/kakaoBtn.png" alt="카카오 로그인"></button>
+      <button class="w-[55px] h-[55px] border-[1px] border-gray-200  rounded-[50px] "><img src="../assets/images/googleBtn.png" alt="구글 로그인"></button>
     </div>
   
     <!-- 모달 컴포넌트 -->
