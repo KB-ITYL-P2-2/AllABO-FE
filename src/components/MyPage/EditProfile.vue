0d3dd9b6697e9524049b130adae5055f6ce4740a
yeon-jae <lisanamyj99@gmail.com>
2024-10-02 15:37:18 +0900

    style: sidebar적용

--- src/components/MyPage/EditProfile.vue
+++ src/components/MyPage/EditProfile.vue
@@ -1,91 +1,83 @@
 <template>
-    <div class="h-[70px] bg-kb-brown-1 mb-[50px]"></div>
-  <div class="mt-[100px] flex justify-center">
-    <img src="../../assets/images/user.png" alt="프로필 이미지" class="justify-center w-32 h-32 rounded-full  mb-[60px]" />
-  </div>
-    <div class="flex justify-center">
-      <!-- 사이드바 -->
-      <div class="w-1/5 p-8 flex flex-col items-center">
-        <ul class="space-y-8 text-lg text-font-color ml-[40px]">
-          <li>
-            <button @click="editProfile" class=" hover:bg-stone-500 hover:text-white hover:rounded-md focus:outline-none text-[20px] p-3">프로필 수정</button>
-          </li>
-          <li>
-            <button @click="editPassword" class="hover:bg-stone-500 hover:text-white hover:rounded-md focus:outline-none text-[20px] p-3">비밀번호 변경</button>
-          </li>
-          <li>
-            <button @click="withdrawMembership" class="hover:bg-stone-500 hover:text-white hover:rounded-md focus:outline-none text-[20px] p-3">회원탈퇴</button>
-          </li>
-        </ul>
-      </div>
-  
-      <!-- 정보 수정 폼 -->
-      <div class="w-4/5 flex flex-col items-center mb-[100px] ml-[100px]">
-        <form @submit.prevent="editForm" class="w-2/3">
-          <!-- 이름 -->
-          <div class="flex flex-col">
-            <label for="name" class="block text-font-color mb-1">이름</label>
-            <input type="text" id="name" v-model="name" placeholder="이름을 입력해주세요" 
-                   class="text-font-color pl-4 h-[50px] w-[500px] rounded-md border border-kb-gray-1 focus:outline-none focus:ring-1 focus:ring-kb-brown-2 transition duration-200 mb-10"/>
-          </div>
-  
-          <!-- 생년월일 -->
-          <div class="flex flex-col">
-            <label for="birthday" class="block text-font-color mb-1">생년월일</label>
-            <input
-              type="text"
-              id="birthday"
-              v-model="birthday"
-              placeholder="YYYY.MM.DD 형식으로 입력해주세요"
-              class="text-font-color pl-4 h-[50px] w-[500px] rounded-md border border-kb-gray-1 focus:outline-none focus:ring-1 focus:ring-kb-brown-2 transition duration-200 mb-10"/>          
+    <div class="h-[70px] bg-kb-brown-1 mb-[160px]"></div>
+  <div class="flex">
+    <SideBar/>
+
+      <div class="flex-col justify-center">
+        <div class="flex justify-center">
+          <img src="../../assets/images/user.png" alt="프로필 이미지" class="justify-center w-32 h-32 rounded-full  ml-[450px] mb-[50px]" />
+        </div>
+        <!-- 정보 수정 폼 -->
+        <div class="w-4/5 flex flex-col items-center mb-[100px] ml-[250px]">
+          <form @submit.prevent="editForm" class="w-2/3">
+            <!-- 이름 -->
+            <div class="flex flex-col">
+              <label for="name" class="block text-font-color mb-1">이름</label>
+              <input type="text" id="name" v-model="name" placeholder="이름을 입력해주세요" 
+                     class="text-font-color pl-4 h-[50px] w-[500px] rounded-md border border-kb-gray-1 focus:outline-none focus:ring-1 focus:ring-kb-brown-2 transition duration-200 mb-10"/>
             </div>
-          <!-- 이메일 -->
-          <div class="flex flex-col">
-            <label for="email" class="block text-font-color mb-1">이메일</label>
-            <input
-              type="email"
-              id="email"
-              v-model="email"
-              placeholder="이메일을 입력해주세요"
-              class="text-font-color pl-4 h-[50px] w-[500px] rounded-md border border-kb-gray-1 focus:outline-none focus:ring-1 focus:ring-kb-brown-2 transition duration-200 mb-10"/>        
-          </div>
-          <!-- 전화번호 -->
-          <div class="flex flex-col">
-            <label for="tel" class="block text-font-color mb-1">전화번호</label>
-            <input
-              type="tel"
-              id="tel"
-              v-model="tel"
-              placeholder="010-1234-1234"
-              class="text-font-color pl-4 h-[50px] w-[500px] rounded-md border border-kb-gray-1 focus:outline-none focus:ring-1 focus:ring-kb-brown-2 transition duration-200"/>         
-          </div>
-  
-          <!-- 버튼 -->
-          <div class="flex mt-8 mr-[20px]">
-            <button
-              type="button"
-              @click="$router.push('/mypage')"
-              class="w-[200px] h-[50px] rounded-[15px] bg-white text-font-color border-kb-gray-1 hover:bg-kb-gray-1 transition duration-200"
-            >
-              취소
-            </button>
-            <button
-              type="submit"
-              :class="[isFormValid ? 'bg-kb-brown-2 text-white hover:bg-kb-yellow-1' : 'bg-gray-300 text-gray-400']"
-              :disabled="!isFormValid"
-              class="w-[200px] h-[50px] rounded-[15px] transition duration-200"
-            >
-              저장
-            </button>
-          </div>
-        </form>
+    
+            <!-- 생년월일 -->
+            <div class="flex flex-col">
+              <label for="birthday" class="block text-font-color mb-1">생년월일</label>
+              <input
+                type="text"
+                id="birthday"
+                v-model="birthday"
+                placeholder="YYYY.MM.DD 형식으로 입력해주세요"
+                class="text-font-color pl-4 h-[50px] w-[500px] rounded-md border border-kb-gray-1 focus:outline-none focus:ring-1 focus:ring-kb-brown-2 transition duration-200 mb-10"/>          
+              </div>
+            <!-- 이메일 -->
+            <div class="flex flex-col">
+              <label for="email" class="block text-font-color mb-1">이메일</label>
+              <input
+                type="email"
+                id="email"
+                v-model="email"
+                placeholder="이메일을 입력해주세요"
+                class="text-font-color pl-4 h-[50px] w-[500px] rounded-md border border-kb-gray-1 focus:outline-none focus:ring-1 focus:ring-kb-brown-2 transition duration-200 mb-10"/>        
+            </div>
+            <!-- 전화번호 -->
+            <div class="flex flex-col">
+              <label for="tel" class="block text-font-color mb-1">전화번호</label>
+              <input
+                type="tel"
+                id="tel"
+                v-model="tel"
+                placeholder="010-1234-1234"
+                class="text-font-color pl-4 h-[50px] w-[500px] rounded-md border border-kb-gray-1 focus:outline-none focus:ring-1 focus:ring-kb-brown-2 transition duration-200"/>         
+            </div>
+    
+            <!-- 버튼 -->
+            <div class="flex mt-8 ml-[80px]">
+              <button
+                type="button"
+                @click="$router.push('/mypage')"
+                class="h-[50px] w-[150px] rounded-md bg-white text-kb-brown-2 border border-kb-brown-2 mr-4 hover:bg-gray-100 transition duration-200"
+              >
+                취소
+              </button>
+              <button
+                type="submit"
+                :class="[isFormValid ? 'bg-kb-brown-2 hover:bg-kb-yellow-1' : 'bg-kb-gray-2']" 
+                      :disabled="!isFormValid" 
+                      class="h-[50px] w-[150px] rounded-md text-white  transition duration-200"
+              >
+                저장
+              </button>
+            </div>
+
+     
+          </form>
+        </div>
       </div>
-    </div>
+  </div>
   </template>
   
   <script setup>
   import { ref, computed } from 'vue';
   import { useRouter } from 'vue-router';
+import SideBar from './SideBar.vue';
 
   // 가라 데이터 
   const name = ref('김가나');
@@ -98,8 +90,7 @@
   const isFormValid = computed(() => {
     return name.value && birthday.value && email.value && tel.value;
   });
-  
-  // 폼 제출 핸들러
+
   const editForm = () => {
     if (isFormValid.value) {
       console.log('폼 제출:', { name: name.value, birthday: birthday.value, email: email.value, tel: tel.value });
@@ -107,12 +98,7 @@
     }
   };
   
-  // 사이드바 메뉴 핸들러
-  const editProfile = () => {
-    router.push('/mypage-profile')
-  }
-  const editPassword = () =>  router.push('/mypage-password')
-  const withdrawMembership = () => router.push('/mypage-withdraw')
+
   </script>
   
   
