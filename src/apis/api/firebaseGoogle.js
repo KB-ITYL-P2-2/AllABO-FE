1d9217bcf35bdf2ccd49761c600add023161b058
yeon-jae <lisanamyj99@gmail.com>
2024-10-05 13:30:41 +0900

    feat: 구글 파이어베스 로그인 토큰, 사용자 정보

--- src/apis/api/firebaseGoogle.js
+++ src/apis/api/firebaseGoogle.js
@@ -4,8 +4,14 @@ import { auth, provider } from "../utils/firebase";
 export const googleLoginHandler = async () => {
   try {
     const result = await signInWithPopup(auth, provider);
-    const idToken = await result.user.getIdToken(); 
-    console.log(idToken)//토큰값 확인
+    //사용자 콘솔 
+    const user = result.user;
+    console.log("사용자 이메일:", user.email);
+    console.log("사용자 이름:", user.displayName);
+    console.log("이메일 확인 여부:", user.emailVerified);
+
+    const idToken = await result.user.getIdToken();
+    console.log(idToken); //토큰값 확인
     return idToken;
   } catch (error) {
     console.error("Google 로그인 실패:", error);
@@ -15,9 +21,12 @@ export const googleLoginHandler = async () => {
 
 export const sendTokenToBackend = async (idToken) => {
   try {
-    const response = await axios.post(`${import.meta.env.VITE_MY_SERVER_URL}/google-login`, {
-      token: idToken,
-    });
+    const response = await axios.post(
+      `${import.meta.env.VITE_MY_SERVER_URL}/google-login`,
+      {
+        token: idToken,
+      }
+    );
     return response.data;
   } catch (error) {
     console.error("백엔드로 토큰 전송 실패:", error);
