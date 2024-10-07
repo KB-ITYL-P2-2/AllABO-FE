3dd67eba84e2b6d911a8c7557a2452c22687084d
Doyeon <duseh729@naver.com>
2024-10-07 17:45:19 +0900

    feat: 전체 상품 ui 구현

---
+++ src/components/AllProducts/ProductItem.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="border-b px-[20%]">
+    <div class="relative flex w-full gap-4 p-5 cursor-pointer hover:bg-gray-50">
+      <!-- 이미지 -->
+      <div class="relative w-20 h-20 ">
+        <div class="absolute w-full h-full rounded-full bg-kb-yellow-3 blur-sm"></div>
+        <img :src="items.url.imageUrl" class="absolute rotate-90 top-[14px]" alt="" />
+      </div>
+
+      <!-- 내용 -->
+      <div class="flex flex-col justify-center">
+        <h1 class="text-[18px]">{{ items.name }}</h1>
+        <h3 class="text-[14px] mt-2 text-kb-gray-1">{{ items.content }}</h3>
+      </div>
+
+      <div class="absolute right-3 top-[30%] flex items-center gap-2">
+        <button class="p-2 ml-4 bg-white rounded-full shadow-lg">
+          <font-awesome-icon :icon="['far', 'heart']" class="text-kb-yellow-3" size="xl" />
+        </button>
+        <button>
+          <font-awesome-icon :icon="['fas', 'chevron-down']" />
+          <!-- <font-awesome-icon :icon="['fas', 'chevron-up']" /> -->
+        </button>
+      </div>
+    </div>
+
+    <!-- 클릭시 콘텐츠 노출 -->
+    <div>
+      <div class="flex">
+        <div v-for="(item, index) in items.data" :key="index" class="flex-1 p-10 text-center" :class="index !== dataLength ? `border-r` : ''">
+          <h3 class="text-kb-gray-1">카테고리</h3>
+          <h3 class="text-[20px] mt-2">{{ item }}</h3>
+        </div>
+      </div>
+
+      <div class="mb-4 text-center"><CommonButton :text="'상세보기'" /></div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import CommonButton from "../common/CommonButton.vue";
+
+const props = defineProps({
+  items: {
+    type: Object,
+    required: true,
+  },
+});
+
+const dataLength = ref(Object.keys(props.items.data).length - 1);
+console.log(dataLength.value);
+</script>
---
+++ src/pages/AllProducts.vue
@@ -0,0 +1,183 @@
+<template>
+  <div>
+    <div class="h-[70px] bg-kb-brown-1"></div>
+
+    <!-- py-16 px-[20%] -->
+    <h1 class="font-bold text-[28px] md:max-laptop:mb-10 mb-24 z-20 text-left bg-kb-yellow-4 h-[160px]" :class="`${LAYOUT_VARIANTS.default}`">{{ `신용카드` }}</h1>
+    <div >
+      
+      <ProductItem v-for="(item, index) in cardData" :key="index" :items="item" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import LAYOUT_VARIANTS from "../constant/layout";
+import ProductItem from "../components/AllProducts/ProductItem.vue";
+
+const cardData = [
+  {
+    name: "첵첵 체크카드(마루는강쥐)",
+    content: "마루와 함께 혜택도 첵첵!",
+    data: {
+      category: "할인",
+    },
+    url: {
+      imageUrl: "https://img1.kbcard.com/ST/img/cxc/kbcard/upload/img/product/01760_img.png",
+      pageUrl: "https://card.kbcard.com/CRD/DVIEW/HCAMCXPRICAC0076?mainCC=a&cooperationcode=01760",
+    },
+  },
+  {
+    name: "첵첵 체크카드(마루는강쥐)",
+    content: "마루와 함께 혜택도 첵첵!",
+    data: {
+      category: "할인",
+    },
+    url: {
+      imageUrl: "https://img1.kbcard.com/ST/img/cxc/kbcard/upload/img/product/01760_img.png",
+      pageUrl: "https://card.kbcard.com/CRD/DVIEW/HCAMCXPRICAC0076?mainCC=a&cooperationcode=01760",
+    },
+  },
+  {
+    name: "첵첵 체크카드(마루는강쥐)",
+    content: "마루와 함께 혜택도 첵첵!",
+    data: {
+      category: "할인",
+    },
+    url: {
+      imageUrl: "https://img1.kbcard.com/ST/img/cxc/kbcard/upload/img/product/01760_img.png",
+      pageUrl: "https://card.kbcard.com/CRD/DVIEW/HCAMCXPRICAC0076?mainCC=a&cooperationcode=01760",
+    },
+  },
+  {
+    name: "첵첵 체크카드(마루는강쥐)",
+    content: "마루와 함께 혜택도 첵첵!",
+    data: {
+      category: "할인",
+    },
+    url: {
+      imageUrl: "https://img1.kbcard.com/ST/img/cxc/kbcard/upload/img/product/01760_img.png",
+      pageUrl: "https://card.kbcard.com/CRD/DVIEW/HCAMCXPRICAC0076?mainCC=a&cooperationcode=01760",
+    },
+  },
+  {
+    name: "첵첵 체크카드(마루는강쥐)",
+    content: "마루와 함께 혜택도 첵첵!",
+    data: {
+      category: "할인",
+    },
+    url: {
+      imageUrl: "https://img1.kbcard.com/ST/img/cxc/kbcard/upload/img/product/01760_img.png",
+      pageUrl: "https://card.kbcard.com/CRD/DVIEW/HCAMCXPRICAC0076?mainCC=a&cooperationcode=01760",
+    },
+  },
+];
+const depositData = [
+  {
+    name: "KB Star 정기예금",
+    data: {
+      content: "Digital KB의 대표 정기예금",
+      category: "예금",
+      date: "1~36개월",
+      interestRate: "연 2.7% ~ 3.4%",
+    },
+    url: {
+      imageUrl: "https://img1.kbcard.com/ST/img/cxc/kbcard/upload/img/product/01760_img.png",
+      pageUrl: "https://obank.kbstar.com/quics?page=C016613&cc=b061496:b061645&isNew=N&prcode=DP01000938",
+    },
+  },
+  {
+    name: "KB Star 정기예금",
+    data: {
+      content: "Digital KB의 대표 정기예금",
+      category: "예금",
+      date: "1~36개월",
+      interestRate: "연 2.7% ~ 3.4%",
+    },
+    url: {
+      imageUrl: "https://img1.kbcard.com/ST/img/cxc/kbcard/upload/img/product/01760_img.png",
+      pageUrl: "https://obank.kbstar.com/quics?page=C016613&cc=b061496:b061645&isNew=N&prcode=DP01000938",
+    },
+  },
+  {
+    name: "KB Star 정기예금",
+    data: {
+      content: "Digital KB의 대표 정기예금",
+      category: "예금",
+      date: "1~36개월",
+      interestRate: "연 2.7% ~ 3.4%",
+    },
+    url: {
+      imageUrl: "https://img1.kbcard.com/ST/img/cxc/kbcard/upload/img/product/01760_img.png",
+      pageUrl: "https://obank.kbstar.com/quics?page=C016613&cc=b061496:b061645&isNew=N&prcode=DP01000938",
+    },
+  },
+  {
+    name: "KB Star 정기예금",
+    data: {
+      content: "Digital KB의 대표 정기예금",
+      category: "예금",
+      date: "1~36개월",
+      interestRate: "연 2.7% ~ 3.4%",
+    },
+    url: {
+      imageUrl: "https://img1.kbcard.com/ST/img/cxc/kbcard/upload/img/product/01760_img.png",
+      pageUrl: "https://obank.kbstar.com/quics?page=C016613&cc=b061496:b061645&isNew=N&prcode=DP01000938",
+    },
+  },
+  {
+    name: "KB Star 정기예금",
+    data: {
+      content: "Digital KB의 대표 정기예금",
+      category: "예금",
+      date: "1~36개월",
+      interestRate: "연 2.7% ~ 3.4%",
+    },
+    url: {
+      imageUrl: "https://img1.kbcard.com/ST/img/cxc/kbcard/upload/img/product/01760_img.png",
+      pageUrl: "https://obank.kbstar.com/quics?page=C016613&cc=b061496:b061645&isNew=N&prcode=DP01000938",
+    },
+  },
+  {
+    name: "KB Star 정기예금",
+    data: {
+      content: "Digital KB의 대표 정기예금",
+      category: "예금",
+      date: "1~36개월",
+      interestRate: "연 2.7% ~ 3.4%",
+    },
+    url: {
+      imageUrl: "https://img1.kbcard.com/ST/img/cxc/kbcard/upload/img/product/01760_img.png",
+      pageUrl: "https://obank.kbstar.com/quics?page=C016613&cc=b061496:b061645&isNew=N&prcode=DP01000938",
+    },
+  },
+];
+const loanData = [
+  {
+    name: "KB 직장인든든 신용대출",
+    data: {
+      content: "직장인이라면",
+      category: "신용대출",
+      amount: "최고 3억원",
+      channel: "스타뱅킹",
+    },
+    url: {
+      imageUrl: "https://img1.kbcard.com/ST/img/cxc/kbcard/upload/img/product/01760_img.png",
+      pageUrl: "https://obank.kbstar.com/quics?page=C103429&cc=b104363:b104516&isNew=N&prcode=LN20000159&QSL=F",
+    },
+  },
+];
+const insuranceData = [
+  {
+    name: "KB자동차보험",
+    data: {
+      content: "보험이에욤",
+      category: "자동차/운전자",
+    },
+    url: {
+      imageUrl: "https://img1.kbcard.com/ST/img/cxc/kbcard/upload/img/product/01760_img.png",
+      pageUrl: "https://www.kbinsure.co.kr/CG301010012.ec",
+    },
+  },
+];
+</script>
