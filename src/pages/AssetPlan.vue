c7b2146a1e880050921372a633d7cdae0285f139
hasesy <kseoy0607@gmail.com>
2024-10-08 17:39:38 +0900

    feat: 보험 리밸런싱 페이지 텍스트

--- src/pages/AssetPlan.vue
+++ src/pages/AssetPlan.vue
@@ -1,4 +1,15 @@
 <template>
-    <div class="h-[90px]">자산 설계 페이지</div>
- </template>
- <script setup></script>
+  <div class="bg-insurance-back top-[490px] h-screen w-full overflow-y-auto">
+    <div class="container mx-auto px-4">
+    <h1 class="text-center text-4xl font-bold mt-[220px] text-font-color">
+      보험 리밸런싱
+    </h1>
+    <p class="text-center text-[12px] text-gray-300 mt-2">
+      제공된 가입 보험 내역을 바탕으로 한 리밸런싱 결과로, 실제 데이터와 차이가
+      있을 수 있습니다.
+    </p>
+</div>
+  </div>
+</template>
+
+<script setup></script>
