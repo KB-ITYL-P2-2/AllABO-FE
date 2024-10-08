3c9c06f412b9742a04b4a43606f8d492d4c3062e
yeon-jae <lisanamyj99@gmail.com>
2024-10-08 15:53:42 +0900

    feat: 저축 분석 페이지 추가

--- src/pages/AssetAnalyze.vue
+++ src/pages/AssetAnalyze.vue
@@ -7,9 +7,12 @@
         <TotalText class="w-1/2" />
       </div>
     </div>
-
+    <!--저축 분석 페이지 -->
+    <div class="section h-screen items-center bg-asset-back">
+      <AnalyzeSavings/>
+    </div>
     <!-- 자산, 부채 부석 페이지 -->
-    <div class="section h-screen  items-center">
+    <div class="section h-screen  items-center bg-asset-back">
       <AnalyzeDebt />
     </div>
 
@@ -19,6 +22,7 @@
 import TotalCircle from "../components/AssetAnalyze/TotalCircle.vue";
 import TotalText from "../components/AssetAnalyze/TotalText.vue";
 import AnalyzeDebt from "../components/AssetAnalyze/AnalyzeDebt.vue";
+import AnalyzeSavings from "../components/AssetAnalyze/AnalyzeSavings.vue";
 
 </script>
 
