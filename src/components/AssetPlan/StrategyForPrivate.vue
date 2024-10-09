3d00432c9962ef6cc65f4b5192e7934c31e51c60
Doyeon <duseh729@naver.com>
2024-10-10 00:20:59 +0900

    feat: 부채관리 솔루션 ui 구성 힘든 시간이었다,,

--- src/components/AssetPlan/StrategyForPrivate.vue
+++ src/components/AssetPlan/StrategyForPrivate.vue
@@ -6,7 +6,7 @@
         <div class="relative flex-1 h-full">
           <template v-for="(item, index) in goalData" :key="index">
             <StrategyCircle
-              :class="scrollY < (index + 1) * 600 && scrollY >= index * 600 ? 'opacity-100' : 'opacity-0'"
+              :class="scrollY < (index + 1) * 400 && scrollY >= index * 400 ? 'opacity-100' : 'opacity-0'"
               class="absolute animation top-[7%] left-0"
               :title="item.title"
               :percent="item.percent"
@@ -16,11 +16,11 @@
 
         <!-- contents -->
         <div class="relative flex-[2] h-full">
-          <StrategyText :class="scrollY < 300 ? 'opacity-100' : 'opacity-0'" class="absolute animation top-[18%] left-0" />
+          <StrategyText :class="scrollY < 200 ? 'opacity-100' : 'opacity-0'" class="absolute animation top-[22%] left-0" />
           <template v-for="(item, index) in recommendData" :key="index">
             <StrategyRecommend
-              :class="scrollY >= (index + 1) * 300 && scrollY < (index + 2) * 300 ? 'opacity-100' : 'opacity-0'"
-              class="absolute top-[22%] animation"
+              :class="scrollY >= (index + 1) * 200 && scrollY < (index + 2) * 200 ? 'opacity-100' : 'opacity-0'"
+              class="absolute top-[26%] animation"
               :title="item.title"
               :products="item.products"
             />
@@ -30,13 +30,30 @@
     </div>
 
     <!-- 부채 관리 솔루션 -->
-    <div class="w-full h-[2000px] bg-kb-yellow-4">
-      <div :class="scrollY > 1100 ? 'opacity-100' : 'opacity-0'" class="relative w-full h-full animation">
-        <div class="absolute top-0 left-[25%] w-[1px] h-full bg-[#6D6262]"></div>
-        <div class="absolute top-0 right-[25%] w-[1px] h-full bg-[#6D6262]"></div>
+    <div class="w-full h-[2000px] bg-kb-yellow-4 relative">
+      <div
+        :class="scrollY > 800 ? 'opacity-100' : 'opacity-0'"
+        class="sticky top-0 border-x-[1px] border-kb-brown-3 h-[100vh] animation flex items-center justify-center"
+      >
+        <div class="flex justify-center h-full py-32 flex-3">
+          <div class="absolute top-0 left-[21.1%] -z-10 w-[1px] h-full bg-[#6D6262]"></div>
+          <div class="flex flex-col items-center justify-between">
+            <StrategyDept class="" :text="'우선순위'" />
+            <StrategyDept class="" :text="'우선순위'" />
+          </div>
+        </div>
 
-        <div class="sticky top-[35%] flex items-center justify-center" ref="circleElement">
+        <!-- 3원 -->
+        <div class="flex items-center justify-center flex-1" ref="circleElement">
           <StrategyCircle :title="'부채관리'" :percent="'솔루션'" />
+        </div>
+
+        <div class="flex justify-center h-full pt-40 pb-12 flex-3">
+          <div class="absolute top-0 right-[21%] -z-10 w-[1px] h-full bg-[#6D6262]"></div>
+          <div class="flex flex-col items-center justify-between">
+            <StrategyDept class="" :text="'우선순위'" />
+            <StrategyDept class="" :text="'우선순위'" />
+          </div>
         </div>
       </div>
     </div>
@@ -49,6 +66,7 @@ import { ref, onMounted, onBeforeUnmount, watch } from "vue";
 import StrategyCircle from "./StrategyForPrivate/StrategyCircle.vue";
 import StrategyText from "./StrategyForPrivate/StrategyText.vue";
 import StrategyRecommend from "./StrategyForPrivate/StrategyRecommend.vue";
+import StrategyDept from "./StrategyForPrivate/StrategyDept.vue";
 
 const goalData = [
   { title: "목표 저축률", percent: `${25}%` },
@@ -64,6 +82,7 @@ const scrollY = ref(0);
 
 const handleScroll = () => {
   scrollY.value = window.scrollY;
+  console.log(scrollY.value);
 };
 
 onMounted(() => {
@@ -77,7 +96,9 @@ onBeforeUnmount(() => {
 
 <style scoped>
 .animation {
-  transform: translateY(20px); /* 아래에서 올라오는 효과 */
   transition: opacity 0.5s, transform 0.5s; /* 부드러운 전환 */
+}
+.flex-3 {
+  flex: 4 1 0; /* flex-grow, flex-shrink, flex-basis */
 }
 </style>
