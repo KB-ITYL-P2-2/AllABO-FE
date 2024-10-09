9aec66ce373681172e2fe203dbeb165417706c2e
Doyeon <duseh729@naver.com>
2024-10-09 18:25:06 +0900

    feat: 전체상품 네비게이터 추가

---
+++ src/components/AssetPlan/StrategyForPrivate.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="w-full h-[calc(100vh-70px)] bg-kb-yellow-4 flex items-center">
+    <div class="h-[400px] bg-kb-yellow-8 w-full">
+      <!-- 3원 -->
+      <div class="rounded-full h-[300px] w-[300px] bg-kb-yellow-5 relative">
+        <div class="absolute w-full h-full translate-x-2 border rounded-full top-[-4px] right-[-4px] border-kb-yellow-2"></div>
+        <div class="absolute w-full h-full border rounded-full border-kb-yellow-2 top-1 right-1"></div>
+      </div>
+
+      <!-- contents -->
+      <div>월 지출을 10% 감소하여 추가 저축 및 투자 비율을 높이는 방안을 고려해야 합니다.</div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
