e3d3154a377beac9a02e3835e067991db2ab67b2
hasesy <kseoy0607@gmail.com>
2024-10-11 17:51:41 +0900

    feat: 배경 원 크기 수정, 위치 수정

--- src/components/Loading/PlanLoading.vue
+++ src/components/Loading/PlanLoading.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="absolute z-[49] w-full h-screen bg-white">
-    <div class="w-[500px] h-[500px] fixed top-[25%] left-[35%] bg-kb-yellow-3 opacity-50 rounded-full blur-3xl -z-10"></div>
+    <div class="absolute z-[49] w-full h-screen bg-white flex items-center justify-center">
+    <div class="w-[700px] h-[700px]  bg-kb-yellow-3 opacity-50 rounded-full blur-3xl -z-10"></div>
     </div>
 </template>
 
