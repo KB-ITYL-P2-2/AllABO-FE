f408439e5125e56e0fb8a98bc5e38ffa22e7b30e
Seoyeon Kim <kseoy0607@gmail.com>
2024-10-16 14:47:16 +0900

    feat: 자산분석 문구 수정

--- src/components/AssetAnalyze/AnalyzeDebt.vue
+++ src/components/AssetAnalyze/AnalyzeDebt.vue
@@ -29,9 +29,9 @@
           >
             총 자산
           </button>
-          <p class="text-[20px] text-kb-brown-1">20대 평균 총 자산보다</p>
+          <p class="text-[20px] text-kb-brown-1">20대 평균 총 자산의</p>
           <p class="text-[36px] font-bold text-font-color mb-2">
-            약 {{ compareTotal }}배 높습니다
+            약 {{ compareTotal }}배 입니다.
           </p>
           <AssetGraph
             :userTotalAssets="userTotalAssets"
@@ -90,10 +90,10 @@
             자산 대비 부채 비율
           </button>
           <p class="text-[20px] text-kb-brown-1">
-            20대 평균 자산 대비 부채 비율보다
+            20대 평균 자산 대비 부채 비율의
           </p>
           <p class="text-[36px] font-bold text-font-color mb-2">
-            약 {{ compareAssetDebt }}% 낮습니다
+            약 {{ compareAssetDebt }}% 입니다.
           </p>
           <DebtGraph
             :assetDebtRatio="assetDebtRatio"
@@ -153,10 +153,10 @@
             연 소득 대비 부채 비율
           </button>
           <p class="text-[20px] text-kb-brown-1">
-            20대 평균 연 소득 대비 부채 비율보다
+            20대 평균 연 소득 대비 부채 비율의
           </p>
           <p class="text-[36px] font-bold text-font-color mb-2">
-            약 {{ compareIncomeDebt }}% 낮습니다
+            약 {{ compareIncomeDebt }}% 입니다.
           </p>
           <IncomeGraph
             :incomeDebtRatio="incomeDebtRatio"
@@ -200,20 +200,20 @@
 </template>
 
 <script setup>
-import axios from "axios";
-import { ref, onBeforeMount } from "vue";
-import { useAuthStore } from "../../stores/auth";
+import axios from 'axios';
+import { ref, onBeforeMount } from 'vue';
+import { useAuthStore } from '../../stores/auth';
 
-import AssetGraph from "./graph/AssetGraph.vue";
-import DebtGraph from "./graph/DebtGraph.vue";
-import IncomeGraph from "./graph/IncomeGraph.vue";
-import { loadingStateStore } from "../../stores/loadingStateStore";
+import AssetGraph from './graph/AssetGraph.vue';
+import DebtGraph from './graph/DebtGraph.vue';
+import IncomeGraph from './graph/IncomeGraph.vue';
+import { loadingStateStore } from '../../stores/loadingStateStore';
 
 // 데이터 상태 관리
 const flipped = ref([false, false, false]);
 
 //연령대
-const age = ref("");
+const age = ref('');
 
 // 비교 데이터 변수들
 const compareTotal = ref(0);
@@ -223,17 +223,17 @@ const compareIncomeDebt = ref(0);
 // 첫 번째 값 (총 자산)
 const userTotalAssets = ref(0);
 const averageTotalAssets = ref(0);
-const assetAnalysis = ref("");
+const assetAnalysis = ref('');
 
 // 두 번째 값 (자산 대비 부채율)
 const assetDebtRatio = ref(0);
 const averageDebtRatio = ref(0);
-const assetDebtAnalysis = ref("");
+const assetDebtAnalysis = ref('');
 
 // 세 번째 값 (연 소득 대비 부채율)
 const incomeDebtRatio = ref(0);
 const averageIncomeRatio = ref(0);
-const incomeDebtAnalysis = ref("");
+const incomeDebtAnalysis = ref('');
 
 // 키워드들
 const assetKeywords = ref([]); // 자산 비교 해시태그
@@ -247,7 +247,7 @@ const fetchAssetLoanData = async () => {
   const authStore = useAuthStore();
   const token = authStore.token;
   if (!token) {
-    console.error("토큰이 없습니다. 로그인 후 다시 시도하세요.");
+    console.error('토큰이 없습니다. 로그인 후 다시 시도하세요.');
     return;
   }
 
@@ -260,7 +260,7 @@ const fetchAssetLoanData = async () => {
       {
         headers: {
           Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
       }
     );
@@ -269,78 +269,78 @@ const fetchAssetLoanData = async () => {
       loadingStore.setIsAssetAnalyzeLoading(false, 1);
     }
 
-    const data = response?.data?.jsonNode?.["같은 연령대 평균과 비교"];
+    const data = response?.data?.jsonNode?.['같은 연령대 평균과 비교'];
     if (!data) {
-      throw new Error("유효한 데이터를 가져오지 못했습니다.");
+      throw new Error('유효한 데이터를 가져오지 못했습니다.');
     }
 
     console.log(data);
 
-    userTotalAssets.value = parseInt(data["자산 비교"]["사용자 자산"]);
+    userTotalAssets.value = parseInt(data['자산 비교']['사용자 자산']);
     averageTotalAssets.value = parseFloat(
-      data["자산 비교"]["같은 연령대 평균 자산(뒤 0000붙여서)"]
+      data['자산 비교']['같은 연령대 평균 자산(뒤 0000붙여서)']
     );
     compareTotal.value = parseFloat(
-      data["자산 비교"]["사용자와 같은연령대 비율 차이(n배)"]
+      data['자산 비교']['사용자와 같은연령대 비율 차이(n배)']
     ).toFixed(2);
-    assetAnalysis.value = data["자산 비교"]["연령 별 자산 총평"].replace(
-      "|",
-      "<br>"
+    assetAnalysis.value = data['자산 비교']['연령 별 자산 총평'].replace(
+      '|',
+      '<br>'
     );
-    assetKeywords.value = data["자산 비교"]["#요약키워드(2개)"];
+    assetKeywords.value = data['자산 비교']['#요약키워드(2개)'];
 
     // 자산 대비 부채 비율 비교
     assetDebtRatio.value = parseFloat(
-      data["자산 대비 부채 비율 비교"]["사용자 자산 대비 부채 비율(% 제외)"]
+      data['자산 대비 부채 비율 비교']['사용자 자산 대비 부채 비율(% 제외)']
     );
     averageDebtRatio.value = parseFloat(
-      data["자산 대비 부채 비율 비교"][
-        "같은 연령대 평균 자산 대비 부채 비율(% 제외,소숫점 두자리수까지)"
+      data['자산 대비 부채 비율 비교'][
+        '같은 연령대 평균 자산 대비 부채 비율(% 제외,소숫점 두자리수까지)'
       ]
     );
     compareAssetDebt.value = Math.abs(
       parseFloat(
-        data["자산 대비 부채 비율 비교"]["사용자와 같은연령대 비율 차이(n배)"]
+        data['자산 대비 부채 비율 비교']['사용자와 같은연령대 비율 차이(n배)']
       )
     ).toFixed(2);
     assetDebtAnalysis.value =
-      data["자산 대비 부채 비율 비교"]["연령 별 자산대비 부채 총평"];
+      data['자산 대비 부채 비율 비교']['연령 별 자산대비 부채 총평'];
     assetDebtKeywords.value =
-      data["자산 대비 부채 비율 비교"]["#요약키워드(2개)"];
+      data['자산 대비 부채 비율 비교']['#요약키워드(2개)'];
 
     // 연 소득 대비 부채 비율 비교
     incomeDebtRatio.value = parseFloat(
-      data["연 소득 대비 부채 비율 비교"][
-        "사용자 연 소득 대비 부채 비율(%제외)"
+      data['연 소득 대비 부채 비율 비교'][
+        '사용자 연 소득 대비 부채 비율(%제외)'
       ]
     );
     averageIncomeRatio.value = parseFloat(
-      data["연 소득 대비 부채 비율 비교"][
-        "같은 연령대 평균 연 소득 대비 부채 비율(% 제외,소숫점 두자리수까지)"
+      data['연 소득 대비 부채 비율 비교'][
+        '같은 연령대 평균 연 소득 대비 부채 비율(% 제외,소숫점 두자리수까지)'
       ]
     );
     compareIncomeDebt.value = Math.abs(
       parseFloat(
-        data["연 소득 대비 부채 비율 비교"][
-          "사용자와 같은연령대 비율 차이(n배)"
+        data['연 소득 대비 부채 비율 비교'][
+          '사용자와 같은연령대 비율 차이(n배)'
         ]
       )
     ).toFixed(2);
     incomeDebtAnalysis.value =
-      data["연 소득 대비 부채 비율 비교"]["연령 별 소득 대비 부채 총평"];
+      data['연 소득 대비 부채 비율 비교']['연령 별 소득 대비 부채 총평'];
     incomeDebtKeywords.value =
-      data["연 소득 대비 부채 비율 비교"]["# 요약키워드(2개)"];
+      data['연 소득 대비 부채 비율 비교']['# 요약키워드(2개)'];
   } catch (error) {
-    console.error("데이터 오류", error);
+    console.error('데이터 오류', error);
   }
 };
 
 onBeforeMount(fetchAssetLoanData); // 데이터 가져오기
 
 // 이미지 경로
-const assetImage1 = "/images/AssetAnalyze/asset1.png";
-const assetImage2 = "/images/AssetAnalyze/asset2.png";
-const assetImage3 = "/images/AssetAnalyze/asset3.png";
+const assetImage1 = '/images/AssetAnalyze/asset1.png';
+const assetImage2 = '/images/AssetAnalyze/asset2.png';
+const assetImage3 = '/images/AssetAnalyze/asset3.png';
 
 // 카드 회전 함수
 const toggleFlip = (index) => {
--- src/pages/AssetPlanSimulation.vue
+++ src/pages/AssetPlanSimulation.vue
@@ -2,7 +2,9 @@
   <div class="h-[calc(100vh-70px)]">
     <div class="h-[70px]"></div>
 
-    <div class="w-[500px] h-[500px] fixed top-[25%] left-[35%] bg-kb-yellow-4 opacity-50 rounded-full blur-3xl -z-10"></div>
+    <div
+      class="w-[500px] h-[500px] fixed top-[25%] left-[35%] bg-kb-yellow-4 opacity-50 rounded-full blur-3xl -z-10"
+    ></div>
 
     <!-- 제목 섹션 -->
     <div class="flex justify-center h-[14%] my-10">
@@ -17,15 +19,25 @@
           <!-- 텍스트 정보 -->
           <div class="flex flex-col items-center w-[45%] justify-center mb-6">
             <!-- 차트 렌더링 -->
-            <div class="w-[100%] h-full p-4 mb-4" :style="{ backgroundColor: item.bgColor }">
+            <div
+              class="w-[100%] h-full p-4 mb-4"
+              :style="{ backgroundColor: item.bgColor }"
+            >
               <Line :data="getChartData(item.title)" :options="chartOptions" />
             </div>
 
             <div class="flex items-center w-full gap-8">
-              <div class="rounded-full border-2 inline-flex flex-col w-[130px] h-[130px] items-center justify-center" :style="{ borderColor: item.hexColor }">
+              <div
+                class="rounded-full border-2 inline-flex flex-col w-[130px] h-[130px] items-center justify-center"
+                :style="{ borderColor: item.hexColor }"
+              >
                 <span>{{ item.title }}</span>
                 <img
-                  :src="`/images/AssetPlanSimulation/${item.title.includes('자산') ? `asset-${item.color}` : `dept-${item.color}`}.png`"
+                  :src="`/images/AssetPlanSimulation/${
+                    item.title.includes('자산')
+                      ? `asset-${item.color}`
+                      : `dept-${item.color}`
+                  }.png`"
                   alt=""
                   class="w-[50px] h-[50px] object-contain"
                 />
@@ -35,7 +47,11 @@
                 <span>현재 전략: {{ item.data.현재_전략 }}</span>
                 <span>개선된 전략: {{ item.data.개선된_전략 }}</span>
                 <div class="h-[1px] bg-black"></div>
-                <span class="text-[20px] font-semibold">{{ `${amountData[index].toLocaleString()}원 ${item.data["상승 여부"] ? "더 상승" : "더 감소"}` }}</span>
+                <span class="text-[20px] font-semibold">{{
+                  `${amountData[index].toLocaleString()}원 ${
+                    item.data['상승 여부'] ? '더 상승' : '더 감소'
+                  }`
+                }}</span>
               </div>
             </div>
           </div>
@@ -45,7 +61,9 @@
 
     <!-- 종합분석 -->
     <div>
-      <div class="flex w-full h-auto mt-[120px] justify-center items-center flex-wrap px-[40%] gap-4">
+      <div
+        class="flex w-full h-auto mt-[120px] justify-center items-center flex-wrap px-[40%] gap-4"
+      >
         <div v-for="(item, index) in ratingData" :key="index">
           <p class="text-[40px]">{{ item }}</p>
         </div>
@@ -59,17 +77,34 @@
 </template>
 
 <script setup>
-import { Line } from "vue-chartjs";
-import { useRouter } from "vue-router";
-import { Chart as ChartJS, Title, Tooltip, Legend, LineElement, CategoryScale, LinearScale, PointElement } from "chart.js";
+import { Line } from 'vue-chartjs';
+import { useRouter } from 'vue-router';
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+} from 'chart.js';
 
-import CommonButton from "../components/common/CommonButton.vue";
-import { onBeforeMount, ref } from "vue";
-import { getAssetAfterYear } from "../apis/api/assetPlan";
-import { loadingStateStore } from "../stores/loadingStateStore";
+import CommonButton from '../components/common/CommonButton.vue';
+import { onBeforeMount, ref } from 'vue';
+import { getAssetAfterYear } from '../apis/api/assetPlan';
+import { loadingStateStore } from '../stores/loadingStateStore';
 
 // ChartJS 플러그인 등록
-ChartJS.register(Title, Tooltip, Legend, LineElement, CategoryScale, LinearScale, PointElement);
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  PointElement
+);
 
 // 종합 분석 데이터
 const ratingData = ref([]);
@@ -89,7 +124,7 @@ const chartOptions = {
   plugins: {
     legend: {
       display: true,
-      position: "top",
+      position: 'top',
     },
   },
   scales: {
@@ -100,7 +135,7 @@ const chartOptions = {
         maxTicksLimit: 5,
         // 만 단위로 포맷팅
         callback: function (value) {
-          return (value / 10000).toLocaleString() + "만";
+          return (value / 10000).toLocaleString() + '만';
         },
       },
     },
@@ -108,37 +143,69 @@ const chartOptions = {
 };
 
 // 각 항목에 맞는 차트 데이터 반환
-const getChartData = title => {
-  if (title === "총 자산") {
+const getChartData = (title) => {
+  if (title === '총 자산') {
     return {
       labels: chartData.value.현재_전략.labels,
       datasets: [
-        { ...chartData.value.현재_전략.datasets[0], borderColor: "#D9D9D9", backgroundColor: "#D9D9D9" },
-        { ...chartData.value.개선된_전략.datasets[0], borderColor: "#F4BF42", backgroundColor: "#F4BF42" },
+        {
+          ...chartData.value.현재_전략.datasets[0],
+          borderColor: '#D9D9D9',
+          backgroundColor: '#D9D9D9',
+        },
+        {
+          ...chartData.value.개선된_전략.datasets[0],
+          borderColor: '#F4BF42',
+          backgroundColor: '#F4BF42',
+        },
       ],
     };
-  } else if (title === "총 부채") {
+  } else if (title === '총 부채') {
     return {
       labels: chartData.value.현재_전략.labels,
       datasets: [
-        { ...chartData.value.현재_전략.datasets[1], borderColor: "#D9D9D9", backgroundColor: "#D9D9D9" },
-        { ...chartData.value.개선된_전략.datasets[1], borderColor: "#FF5252", backgroundColor: "#FF5252" },
+        {
+          ...chartData.value.현재_전략.datasets[1],
+          borderColor: '#D9D9D9',
+          backgroundColor: '#D9D9D9',
+        },
+        {
+          ...chartData.value.개선된_전략.datasets[1],
+          borderColor: '#FF5252',
+          backgroundColor: '#FF5252',
+        },
       ],
     };
-  } else if (title === "순 자산") {
+  } else if (title === '순 자산') {
     return {
       labels: chartData.value.현재_전략.labels,
       datasets: [
-        { ...chartData.value.현재_전략.datasets[2], borderColor: "#D9D9D9", backgroundColor: "#D9D9D9" },
-        { ...chartData.value.개선된_전략.datasets[2], borderColor: "#58AEFF", backgroundColor: "#58AEFF" },
+        {
+          ...chartData.value.현재_전략.datasets[2],
+          borderColor: '#D9D9D9',
+          backgroundColor: '#D9D9D9',
+        },
+        {
+          ...chartData.value.개선된_전략.datasets[2],
+          borderColor: '#58AEFF',
+          backgroundColor: '#58AEFF',
+        },
       ],
     };
-  } else if (title === "총 투자금액") {
+  } else if (title === '총 투자금액') {
     return {
       labels: chartData.value.현재_전략.labels,
       datasets: [
-        { ...chartData.value.현재_전략.datasets[3], borderColor: "#D9D9D9", backgroundColor: "#D9D9D9" },
-        { ...chartData.value.개선된_전략.datasets[3], borderColor: "#B1EF5A", backgroundColor: "#B1EF5A" },
+        {
+          ...chartData.value.현재_전략.datasets[3],
+          borderColor: '#D9D9D9',
+          backgroundColor: '#D9D9D9',
+        },
+        {
+          ...chartData.value.개선된_전략.datasets[3],
+          borderColor: '#B1EF5A',
+          backgroundColor: '#B1EF5A',
+        },
       ],
     };
   }
@@ -147,7 +214,7 @@ const getChartData = title => {
 const router = useRouter();
 
 const goToAssetPlan = () => {
-  router.push("/asset-plan");
+  router.push('/asset-plan');
 };
 
 const loadingStore = loadingStateStore();
@@ -163,54 +230,54 @@ onBeforeMount(async () => {
   if (afterYearData.status == 200) {
     loadingStore.setIsAssetPlanLoadingState(false);
 
-    const temp = afterYearData.data["전략_비교_분석"];
-    chartData.value = afterYearData.data["그래프_데이터"];
-    ratingData.value = temp["종합_평가(#키워드)"].split(" ");
+    const temp = afterYearData.data['전략_비교_분석'];
+    chartData.value = afterYearData.data['그래프_데이터'];
+    ratingData.value = temp['종합_평가(#키워드)'].split(' ');
     simulationData.value = [
       {
-        title: "총 자산",
-        data: { ...temp["총_자산_변화"] },
-        color: "yellow",
-        hexColor: "#F4BF42",
-        bgColor: "#FFFBE7",
+        title: '총 자산',
+        data: { ...temp['총_자산_변화'] },
+        color: 'yellow',
+        hexColor: '#F4BF42',
+        bgColor: '#FFFBE7',
       },
       {
-        title: "총 부채",
-        data: { ...temp["부채_변화"] },
-        color: "pink",
-        hexColor: "#FFAAAA",
-        bgColor: "#FFF6FA",
+        title: '총 부채',
+        data: { ...temp['부채_변화'] },
+        color: 'pink',
+        hexColor: '#FFAAAA',
+        bgColor: '#FFF6FA',
       },
       {
-        title: "순 자산",
-        data: { ...temp["순_자산_변화"] },
-        color: "blue",
-        hexColor: "#8AC6FF",
-        bgColor: "#EEFCFF",
+        title: '순 자산',
+        data: { ...temp['순_자산_변화'] },
+        color: 'blue',
+        hexColor: '#8AC6FF',
+        bgColor: '#EEFCFF',
       },
       {
-        title: "총 투자금액",
-        data: { ...temp["투자_성과"] },
-        color: "green",
-        hexColor: "#B1EF5A",
-        bgColor: "#F0FFEE",
+        title: '총 투자금액',
+        data: { ...temp['투자_성과'] },
+        color: 'green',
+        hexColor: '#B1EF5A',
+        bgColor: '#F0FFEE',
       },
     ];
     // console.log(chartData.value);
     // console.log(ratingData.value);
     // console.log(simulationData.value);
-    const nowAmount = []
+    const nowAmount = [];
     // console.log(chartData.value)
     // console.log(chartData.value.개선된_전략.datasets)
-    for(let c of chartData.value.개선된_전략.datasets){
-      nowAmount.push(c.data[4])
+    for (let c of chartData.value.개선된_전략.datasets) {
+      nowAmount.push(c.data[4]);
     }
-    const planAmount=[]
-    for(let c of chartData.value.현재_전략.datasets){
-      planAmount.push(c.data[4])
+    const planAmount = [];
+    for (let c of chartData.value.현재_전략.datasets) {
+      planAmount.push(c.data[4]);
     }
-    for(let i=0; i<=4; i++){
-      amountData.value.push(nowAmount[i]-planAmount[i]);
+    for (let i = 0; i <= 4; i++) {
+      amountData.value.push(nowAmount[i] - planAmount[i]);
     }
   }
 });
