28c115434dcc3eee45e7bc940c0e49f5c5678057
Doyeon <duseh729@naver.com>
2024-10-16 09:38:53 +0900

    feat: 자산 설계 로딩 보이게 구현

--- src/apis/api/favorites.js
+++ src/apis/api/favorites.js
@@ -1,5 +1,16 @@
 import { authInstance } from "../utils/instance"
 
+export const addFavorite = async(productId, productNum)=>{
+  const response = await authInstance.post(`${import.meta.env.VITE_MY_SERVER_URL}/api/favorites/add`)
+  return response;
+}
+
+export const removeFavorite = async(productId, productNum)=>{
+  const response = await authInstance.post(`${import.meta.env.VITE_MY_SERVER_URL}/api/favorites/remove`)
+  return response;
+
+}
+
 export const getMyFavoritesList = async()=>{
   const response = await authInstance.get(`${import.meta.env.VITE_MY_SERVER_URL}/api/favorites/list`)
   return response
